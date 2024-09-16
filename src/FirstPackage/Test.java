4ab74314f349842aaf748485a4c0385946c97f9c
VanshQA <vanshkumar.tester@gmail.com>
2024-09-16 17:30:35 +0530

    new update

--- src/FirstPackage/Test.java
+++ src/FirstPackage/Test.java
@@ -1,10 +1,13 @@
 package FirstPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https/:www.flipkart");
 	
-	int c=20+30;
-	System.out.println("Hello world");
-	System.out.println(c);
+	
 }
 }
